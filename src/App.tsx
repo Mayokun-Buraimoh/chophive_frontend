02ca2799c832b7c1d78351f952bd0c04e0cb11a8
Adetola Beloved <belovedadetola@gmail.com>
2026-01-07 23:30:39 +0100

    changed back to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
 import Cart from "./components/Cart";
 import Home from "./pages/Landing/Home";
@@ -26,7 +26,7 @@ function App() {
     <AuthProvider>
       <CartProvider>
         <FoodProvider>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
@@ -56,7 +56,7 @@ function App() {
               </Route>
             </Routes>
             <Cart />
-          </BrowserRouter>
+          </HashRouter>
         </FoodProvider>
       </CartProvider>
     </AuthProvider>
